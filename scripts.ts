840bccc6e6d61e7b01c4778f9a755931b90c5dfa
Dan Montgomery <dmontgomery@phase2technology.com>
2023-11-28 10:40:06 -0600

    use JS modules and split TS files

--- scripts.ts
+++ scripts.ts
@@ -1,22 +1,4 @@
-interface WeatherCondition {
-  type: string
-  coverage: string
-}
-
-// @todo set the temperatures as optional so we can handle those better.
-interface ForecastPoint {
-  startTime: string
-  endTime: string
-  temperature: number
-  dewPoint: number
-  windChill: number
-  probabilityOfPercipitation: number
-  weatherCondition: {
-    type: string
-    coverage: string
-  }
-}
-
+import { ForecastPoint, fetchWeatherForecastData } from './weatherGovApi.js'
 
 let forecastPoints: ForecastPoint[] = []
 let selectedForecastPointsPerScreen = 24
@@ -434,90 +416,6 @@ const drawSVG = () => {
   svg.appendChild(buttonContainer)
 
   weatherForecastContainer.appendChild(svg)
-}
-
-const fetchWeatherForecastData = () => {
-  const xhr = new XMLHttpRequest()
-  
-  const promise: Promise<ForecastPoint[]> = new Promise((resolve, reject) => {
-    xhr.onload = () => {
-      forecastPoints = getForecastPointsFromXML()
-      resolve(forecastPoints)
-    }
-  })
-
-  const getForecastPointsFromXML = (): ForecastPoint[] => {
-    const xmlToArray = (q:string) => {
-      if (xhr.responseXML === null) {
-        return []
-      }
-      return Array.from(xhr.responseXML.documentElement.querySelectorAll(q))
-    }
-
-    const temperatures = xmlToArray('temperature[type="hourly"] > value')
-      .map(n => parseInt(n.innerHTML))
-
-    const dewPoints = xmlToArray('temperature[type="dew point"] > value')
-      .map(n => parseInt(n.innerHTML))
-    
-    const windChills = xmlToArray('temperature[type="wind chill"] > value')
-      .map(n => parseInt(n.innerHTML))
-    
-    const probabilityOfPercipitations = xmlToArray('probability-of-precipitation > value')
-      .map(n => parseInt(n.innerHTML))
-
-    const weatherConditions = xmlToArray('weather-conditions')
-      .map(n => {
-        const value = n.querySelector('value')
-        if (!value) {
-          return {
-            type: 'none',
-            coverage: 'none',
-          }
-        }
-
-        return {
-          type: value.getAttribute('weather-type') ?? 'none',
-          coverage: value.getAttribute('coverage') ?? 'none',
-        }
-      })
-
-    const startTimes = xmlToArray('start-valid-time')
-      .map(n => n.innerHTML)
-
-    const endTimes = xmlToArray('end-valid-time')
-      .map(n => n.innerHTML)
-
-    return startTimes.map((startTime, i) => ({
-      startTime,
-      endTime: endTimes[i],
-      temperature: temperatures[i],
-      dewPoint: dewPoints[i],
-      windChill: windChills[i],
-      probabilityOfPercipitation: probabilityOfPercipitations[i],
-      weatherCondition: weatherConditions[i],
-    }))
-  }
-
-  const getLatitudeAndLongitude = (): [number, number] => {
-    const queryParameters = new URLSearchParams(window.location.search)
-
-    const lat = parseFloat(queryParameters.get('lat') ?? '')
-    const lon = parseFloat(queryParameters.get('lon') ?? '')
-
-    return [
-      !isNaN(lat) ? lat : 41.9536,
-      !isNaN(lon) ? lon : -87.7117,
-    ]
-  }
-
-  const [latitude, longitude] = getLatitudeAndLongitude()
-
-  xhr.open('GET',`https://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude}&FcstType=digitalDWML`, true)
-  xhr.responseType = 'document'
-  xhr.send()
-
-  return promise
 }
 
 fetchWeatherForecastData().then(points => {
--- weatherGovApi.js
+++ weatherGovApi.js
@@ -1,2 +1,68 @@
-"use strict";
+export const fetchWeatherForecastData = () => {
+    const xhr = new XMLHttpRequest();
+    const promise = new Promise((resolve, reject) => {
+        xhr.onload = () => {
+            resolve(getForecastPointsFromXML());
+        };
+    });
+    const getForecastPointsFromXML = () => {
+        const xmlToArray = (q) => {
+            if (xhr.responseXML === null) {
+                return [];
+            }
+            return Array.from(xhr.responseXML.documentElement.querySelectorAll(q));
+        };
+        const temperatures = xmlToArray('temperature[type="hourly"] > value')
+            .map(n => parseInt(n.innerHTML));
+        const dewPoints = xmlToArray('temperature[type="dew point"] > value')
+            .map(n => parseInt(n.innerHTML));
+        const windChills = xmlToArray('temperature[type="wind chill"] > value')
+            .map(n => parseInt(n.innerHTML));
+        const probabilityOfPercipitations = xmlToArray('probability-of-precipitation > value')
+            .map(n => parseInt(n.innerHTML));
+        const weatherConditions = xmlToArray('weather-conditions')
+            .map(n => {
+            var _a, _b;
+            const value = n.querySelector('value');
+            if (!value) {
+                return {
+                    type: 'none',
+                    coverage: 'none',
+                };
+            }
+            return {
+                type: (_a = value.getAttribute('weather-type')) !== null && _a !== void 0 ? _a : 'none',
+                coverage: (_b = value.getAttribute('coverage')) !== null && _b !== void 0 ? _b : 'none',
+            };
+        });
+        const startTimes = xmlToArray('start-valid-time')
+            .map(n => n.innerHTML);
+        const endTimes = xmlToArray('end-valid-time')
+            .map(n => n.innerHTML);
+        return startTimes.map((startTime, i) => ({
+            startTime,
+            endTime: endTimes[i],
+            temperature: temperatures[i],
+            dewPoint: dewPoints[i],
+            windChill: windChills[i],
+            probabilityOfPercipitation: probabilityOfPercipitations[i],
+            weatherCondition: weatherConditions[i],
+        }));
+    };
+    const getLatitudeAndLongitude = () => {
+        var _a, _b;
+        const queryParameters = new URLSearchParams(window.location.search);
+        const lat = parseFloat((_a = queryParameters.get('lat')) !== null && _a !== void 0 ? _a : '');
+        const lon = parseFloat((_b = queryParameters.get('lon')) !== null && _b !== void 0 ? _b : '');
+        return [
+            !isNaN(lat) ? lat : 41.9536,
+            !isNaN(lon) ? lon : -87.7117,
+        ];
+    };
+    const [latitude, longitude] = getLatitudeAndLongitude();
+    xhr.open('GET', `https://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude}&FcstType=digitalDWML`, true);
+    xhr.responseType = 'document';
+    xhr.send();
+    return promise;
+};
 //# sourceMappingURL=weatherGovApi.js.map
