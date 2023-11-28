770b16477e2cd87981646dd444d0ed00a649848b
Dan Montgomery <dmontgomery@phase2technology.com>
2023-11-28 16:14:50 -0600

    refactor legend to use similar component pattern

--- src/components/legend.ts
+++ src/components/legend.ts
@@ -1,5 +1,26 @@
 import { CanvasBounds } from '../app.js'
 
+const LegendLabel = ({
+  label,
+  className,
+  offset,
+}: {
+  label: string
+  className: string
+  offset?: number
+}): SVGElement => {
+  const legendLabel = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+  legendLabel.innerHTML = label
+  legendLabel.classList.add('label')
+  legendLabel.classList.add(className)
+
+  if (offset) {
+    legendLabel.setAttribute('transform', `translate(${offset} 0)`)
+  }
+
+  return legendLabel
+}
+
 export const Legend = ({
   canvasBounds,
   canvasPadding,
@@ -11,30 +32,32 @@ export const Legend = ({
   legend.classList.add('legend')
   legend.setAttribute('transform', `translate(${canvasPadding} ${canvasBounds.y.max - (canvasPadding / 2) + 15})`);
 
-  const addLegendLabel = ({label, className, offset}: {
-    label: string
-    className: string
-    offset?: number
-  }) => {
-    const labelElement = document.createElementNS('http://www.w3.org/2000/svg', 'text')
-    labelElement.innerHTML = label
-    labelElement.classList.add('label')
-    labelElement.classList.add(className)
-
-    if (offset) {
-      labelElement.setAttribute('transform', `translate(${offset} 0)`)
-    }
-
-    legend.appendChild(labelElement)
-  }
-
   const legendLabelWidth = 200
 
-  addLegendLabel({label: 'Temperature', className: 'temperature'})
-  addLegendLabel({label: 'Dew Point', className: 'dew-point', offset: legendLabelWidth})
-  addLegendLabel({label: 'Wind Chill', className: 'wind-chill', offset: legendLabelWidth * 2})
-  addLegendLabel({label: 'Snow', className: 'snow', offset: legendLabelWidth * 3})
-  addLegendLabel({label: 'Rain', className: 'rain', offset: legendLabelWidth * 4})
+  legend.appendChild(LegendLabel({
+    label: 'Temperature',
+    className: 'temperature',
+  }))
+  legend.appendChild(LegendLabel({
+    label: 'Dew Point',
+    className: 'dew-point',
+    offset: legendLabelWidth,
+  }))
+  legend.appendChild(LegendLabel({
+    label: 'Wind Chill',
+    className: 'wind-chill',
+    offset: legendLabelWidth * 2,
+  }))
+  legend.appendChild(LegendLabel({
+    label: 'Snow',
+    className: 'snow',
+    offset: legendLabelWidth * 3,
+  }))
+  legend.appendChild(LegendLabel({
+    label: 'Rain',
+    className: 'rain',
+    offset: legendLabelWidth * 4,
+  }))
 
   return legend
 }
