9e0d7ed1fc6a8de358e37193e21d523ef71b3d43
Dan Montgomery <dan@montgomery.onl>
2024-07-28 11:30:34 -0500

    Only show condition line if there are some values.

--- src/components/precipitationLine.ts
+++ src/components/precipitationLine.ts
@@ -13,25 +13,27 @@ export const PrecipitationLine = ({
   className: string
   forecastPoints: ForecastPoint[]
   canvasBounds: CanvasBounds
-}): SVGPolylineElement => Polyline({
-  points: forecastPoints.map((forecastPoint, i) => {
-    const weatherConditions = forecastPoint.weatherConditions
-
-    let value = 0;
-
-    weatherConditions.forEach(weatherCondition => {
-      if (
+}): SVGPolylineElement => {
+  const pointValues = forecastPoints.map((forecastPoint) => {
+    const condition = forecastPoint.weatherConditions
+      .find(weatherCondition => 
         weatherCondition.type === propertyName &&
         precipitationCoverageLabelToValueMap.has(weatherCondition.coverage)
-      ) {
-        value = (precipitationCoverageLabelToValueMap.get(weatherCondition.coverage) as PrecipitationCoverageValue).value
-      }
-    })
+      )
     
-    return {
+    return condition?.coverage ? (precipitationCoverageLabelToValueMap.get(condition.coverage) as PrecipitationCoverageValue).value : 0
+  })
+
+  // We don't want to draw the line if there are no values.
+  const points: {x: number, y: number }[] = pointValues.filter(value => value > 0).length > 0
+    ? pointValues.map((value, i) => ({
       x: canvasBounds.x.min + canvasBounds.x.length * (i / (forecastPoints.length - 1)),
-      y: canvasBounds.y.max - canvasBounds.y.length * (value / (precipitationCoverageLabelToValueMap.size - 1))
-    }
-  }),
-  className
-})
+      y: canvasBounds.y.max - canvasBounds.y.length * (value / (precipitationCoverageLabelToValueMap.size - 1)),
+    }))
+    : []
+
+  return Polyline({
+    points,
+    className,
+  })
+}
